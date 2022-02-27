25ff861fdd8238308d30b3b36d565bb231708048
jlammilliman <jlammilliman@gmail.com>
2022-02-27 00:13:35 -0500

    Worm

--- dbinit/init.sql
+++ dbinit/init.sql
@@ -1,32 +1,13 @@
-CREATE DATABASE IF NOT EXISTS patientsdb;
+-- All code to create/propgate tables should be called within here
 
-USE patientsdb;
+CREATE DATABASE IF NOT EXISTS smartcal_mysqldb;
 
-DROP TABLE IF EXISTS patients;
+USE smartcal_mysqldb;
 
-CREATE TABLE patients (
-  id         BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(255) DEFAULT NULL,
-  last_name  VARCHAR(255) DEFAULT NULL,
-  email      VARCHAR(255) DEFAULT NULL,
-  phone      VARCHAR(255) DEFAULT NULL,
-  address    VARCHAR(255) DEFAULT NULL,
-  diagnosis  VARCHAR(255) DEFAULT NULL,
-  image_url  VARCHAR(255) DEFAULT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (id),
-  CONSTRAINT UQ_Patients_Email UNIQUE (email)
+-- TODO : Replace with a functional login schema, this is for rapid-prototyping purposes
+DROP TABLE IF EXISTS login;
+CREATE TABLE login (
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY (username)
 );
-
-DELIMITER //
-CREATE PROCEDURE create_and_return(IN first_name VARCHAR(255), IN last_name VARCHAR(255), 
-                  IN email VARCHAR(255), IN phone VARCHAR(255), IN address VARCHAR(255), IN diagnosis VARCHAR(255),
-                  IN image_url VARCHAR(255))
-BEGIN
-  INSERT INTO patients(first_name, last_name, email, phone, address, diagnosis, image_url) VALUES (first_name, last_name, email, phone, address, diagnosis, image_url);
-  
-  SET @PATIENT_ID = LAST_INSERT_ID();
-
-  SELECT * FROM patients WHERE id=@PATIENT_ID;
-END //
-DELIMITER ;
