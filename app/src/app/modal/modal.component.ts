53c7236cb4ef88bc0602a6c6df4885a5e12bc078
SRuss76 <98478254+SRuss76@users.noreply.github.com>
2022-04-03 14:58:16 -0400

    Add Events Modal Working
    Can add event using add event modal

--- app/src/app/modal/modal.component.ts
+++ app/src/app/modal/modal.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {SenderService} from "../sender.service";
+
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+  styleUrls: ['./modal.component.css'],
 })
 export class ModalComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+      private router: Router,
+      private service: SenderService,
+  ) { 
+  }
 
   ngOnInit(): void {
   }
 
+  getNewEvent() {
+    let title = (document.getElementById("title") as HTMLInputElement).value;
+    if (title) {
+      let startDate = new Date((document.getElementById("start") as HTMLInputElement).value);
+      let endDate = new Date((document.getElementById("end") as HTMLInputElement).value);
+      this.service.title = title;
+      this.service.start = startDate;
+      this.service.end = endDate;
+      (document.getElementById("form") as HTMLFormElement).reset();
+      this.service.addEvent(title);
+    }
+  }
 }
