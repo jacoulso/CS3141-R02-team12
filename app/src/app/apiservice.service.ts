f237d6b9659e6912a53a998a3ee1088eb4e1b9ba
Justin Milliman <jlammilliman@gmail.com>
2022-04-04 12:20:10 -0400

    added singup function to apiservice

--- app/src/app/apiservice.service.ts
+++ app/src/app/apiservice.service.ts
@@ -7,15 +7,22 @@ import { Observable } from 'rxjs';
 })
 export class ApiserviceService {
 
-  constructor(private _http:HttpClient) { }
+  constructor(private _http: HttpClient) { }
 
   // connect frontend to backend 
   apiUrl = "http://localhost:3000";
   loginAuthPath = this.apiUrl + "/login";
+  registerPath = this.apiUrl + "/singup";
 
-   // login autherization
-   getUserAuth(userCred: string, password: string):Observable<any> {
+  // login autherization
+  getUserAuth(userCred: string, password: string): Observable<any> {
     console.log(`req ==> '${userCred}', '${password}'`);
     return this._http.get(this.loginAuthPath + `/${userCred}/${password}`);
+  }
+
+  // Register a new user
+  registerUser(userCred: string, password: string, email: string): Observable<any> {
+    console.log(`Creating user...`);
+    return this._http.get(this.registerPath + `/${userCred}/${password}/${email}`);
   }
 }
