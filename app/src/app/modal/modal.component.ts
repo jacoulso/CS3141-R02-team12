e937f22669af5ce656176466afd6fd980edfdfa5
SRuss76 <98478254+SRuss76@users.noreply.github.com>
2022-04-08 11:55:49 -0400

    Calendar and Events Stuffs

--- app/src/app/modal/modal.component.ts
+++ app/src/app/modal/modal.component.ts
@@ -2,18 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import {SenderService} from "../sender.service";
 
-
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css'],
 })
 export class ModalComponent implements OnInit {
-
   constructor(
       private router: Router,
       private service: SenderService,
-  ) { 
+  ) {
   }
 
   ngOnInit(): void {
@@ -22,13 +20,32 @@ export class ModalComponent implements OnInit {
   getNewEvent() {
     let title = (document.getElementById("title") as HTMLInputElement).value;
     if (title) {
+      let priority = (document.getElementById("priority") as HTMLInputElement).value;
+      let allDay = (document.getElementById("allDay") as HTMLInputElement).checked;
       let startDate = new Date((document.getElementById("start") as HTMLInputElement).value);
       let endDate = new Date((document.getElementById("end") as HTMLInputElement).value);
-      this.service.title = title;
-      this.service.start = startDate;
-      this.service.end = endDate;
-      (document.getElementById("form") as HTMLFormElement).reset();
-      this.service.addEvent(title);
+      if (endDate >= startDate) {
+        this.service.title = title;
+        this.service.priority = parseInt(priority);
+        this.service.allDay = allDay;
+        this.service.start = startDate;
+        this.service.end = endDate;
+        (document.getElementById("form") as HTMLFormElement).reset();
+        this.service.addEvent(title);
+      }
+      else {
+        alert("End is before Start");
+      }
+
+    }
+  }
+
+  getICS() {
+    let ics = (document.getElementById("myfile") as HTMLInputElement).value;
+    if (ics) {
+      this.service.url = ics;
+      (document.getElementById("importForm") as HTMLFormElement).reset();
+      this.service.sendString("url");
     }
   }
 }
